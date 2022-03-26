c42ab7f1a293480308bd5f18ae58eaef5c7a10d8
saini1998 <sainiaaryaman1998@gmail.com>
2022-03-26 20:34:32 +0000

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Aaryaman Saini on 26/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
